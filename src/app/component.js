3a39e2b83b4bf1140c8695c623d61d53354a16cf
tofferman <tofferman@bol.com>
2023-12-02 16:39:30 +0100

    main: images

--- src/app/component.js
+++ src/app/component.js
@@ -6,7 +6,7 @@ import { useContinueHook } from './continue-hook';
 export default function Component() {
     useContinueHook('step-0', 'stap-1');
 
-    return (<div className="" style={{ left: '20%', top: '20%'}}>
-       
+    return (<div className="absolute" style={{ left: '20%', top: '20%'}}>
+        <img className={`w-3/5 mt-6 transition-opacity ease-in duration-700 opacity-100 inline-block`} src="frame.png" />`
     </div>)
 }
