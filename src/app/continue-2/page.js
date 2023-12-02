d31dfe33dadb826ebc842ca73e92d753e80c85e4
tofferman <tofferman@bol.com>
2023-12-02 17:24:50 +0100

    fix

--- src/app/continue-2/page.js
+++ src/app/continue-2/page.js
@@ -5,7 +5,7 @@ export default function Page() {
     return (
         <main className="flex min-h-screen flex-col items-center p-24">
             <h2>Deel 2</h2>
-            <h2>Heb geduld Marieke tot de volledige text is verschenen druk dan pas op doorgaan!</h2>
+            <h2>Ook hier wachten we even met klikken!</h2>
             <ContinueComponent step={'step-2'} next={'/answers-stap-3'}/>
         </main>
     )
